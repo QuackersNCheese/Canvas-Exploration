e98329c03b6f965276b91892f348d945cbc925c4
QuackersNCheese <Quacktastrophe@protonmail.com>
2024-12-27 14:13:11 -0700

    initial commit

---
+++ tutorial_canvas.js
@@ -0,0 +1,18 @@
+function draw() {
+  const canvas = document.getElementById("tutorial");
+  if (canvas.getContext) {
+    const ctx = canvas.getContext("2d");
+    
+    ctx.fillStyle = "rgba(200, 0, 0, 0.5)"; //(red, green, blue, alpha)
+    ctx.fillRect(10, 10, 50, 50); //(x, y, width, height)
+    
+    ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+    ctx.fillRect(30, 30, 50, 50);
+
+  } else {
+    const para = document.querySelector(".unsupported");
+    para.textContent = "Your browser does not support HTML5 Canvas";
+  }
+}
+
+window.addEventListener("load", draw, false);
