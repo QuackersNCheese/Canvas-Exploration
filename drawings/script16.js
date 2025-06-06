28b51711929881c82a033a17821f119de2769e2c
QuackersNCheese <Quacktastrophe@protonmail.com>
2025-06-05 20:41:33 -0600

    refactored 3D functions to include eyepoint, center of interest, and viewing angle control

--- drawings/script16.js
+++ drawings/script16.js
@@ -3,22 +3,25 @@ const ctx = canvas.getContext('2d');
 
 var x_mid = canvas.width / 2;
 var y_mid = canvas.height / 2;
-var AR = 1;
+var AR = canvas.width/canvas.height;
 var NEAR = 0.1;
 var FAR = 10000.0;
 
 function transform(A, v) {
-  const x = A[0][0] * v[0] + A[0][1] * v[1] + A[0][2] * v[2] + A[0][3];
-  const y = A[1][0] * v[0] + A[1][1] * v[1] + A[1][2] * v[2] + A[1][3];
-  const z = A[2][0] * v[0] + A[2][1] * v[1] + A[2][2] * v[2] + A[2][3];
-  return [x, y, z];
+  const x = A[0][0] * v[0] + A[0][1] * v[1] + A[0][2] * v[2] + A[0][3] * v[3];
+  const y = A[1][0] * v[0] + A[1][1] * v[1] + A[1][2] * v[2] + A[1][3] * v[3];
+  const z = A[2][0] * v[0] + A[2][1] * v[1] + A[2][2] * v[2] + A[2][3] * v[3];
+  const p = A[3][0] * v[0] + A[3][1] * v[1] + A[3][2] * v[2] + A[3][3] * v[3]
+  return [x, y, z, p];
 }
 
 function plot3D(pos, eyepoint, centerOfInterest, viewingAngle) {
-  let [x, y, z] = pos;
+  let [x, y, z, p] = pos;
   let [epx, epy, epz] = eyepoint;
   let [coix, coiy, coiz] = centerOfInterest;
-  let va = viewingAngle;
+  let cot2 = Math.cos(viewingAngle/2) / Math.sin(viewingAngle/2);
+  let alpha = FAR/(FAR-NEAR);
+  let beta=-FAR*NEAR/(FAR-NEAR);
   // create components of vector from eyepoint to center of interest
   let [a,b,c] = [coix-epx, coiy-epy, coiz-epz];
   let cosTheta = c / (a*a + c*c)**0.5;
@@ -26,20 +29,11 @@ function plot3D(pos, eyepoint, centerOfInterest, viewingAngle) {
   let d = (a*a + b*b + c*c)**0.5;     // length of ep to coi vector
   let cosPhi = (a*a + c*c)**0.5 / d;
   let sinPhi = b / d;
-  let A = [[1, 0, 0, -coix], // translate eyepoint to origin
-          [0, 1, 0, -coiy], 
-          [0, 0, 1, -coiz]] 
-  let B = [[cosTheta, 0, -sinTheta,0],  // rotate about Y-axis by angle theta
-          [0, 1, 0, 0],
-          [sinTheta, 0, cosTheta, 0]];
-  let C = [[1, 0, 0, 0],  // rotate about X-axis by angle phi
-          [0, cosPhi, -sinPhi, 0],
-          [0, sinPhi, cosPhi, 0]];
-  let D = [[1, 0, 0, 0],  // flip to a left-handed space with the origin located at the eyepoint?
-          [0, 1, 0, 0],
-          [0, 0, -1, d]];
-
-  let [xprime, yprime, zprime] = transform(D,transform(C,transform(B,transform(A, [x, y, z]))));
+  let Z = [[cosTheta*cot2,0,-sinTheta*cot2,-coix*cosTheta*cot2+coiz*sinTheta*cot2],
+          [-sinTheta*sinPhi*AR*cot2, cosPhi*AR*cot2, -cosTheta*sinPhi*AR*cot2,coix*sinTheta*sinPhi*AR*cot2-coiy*cosPhi*AR*cot2+coiz*cosTheta*sinPhi*AR*cot2],
+          [-sinTheta*cosPhi*alpha,-sinPhi*alpha,-cosTheta*cosPhi*alpha,coix*sinTheta*cosPhi*alpha+coiy*sinPhi*alpha+coiz*cosTheta*cosPhi*alpha+alpha*d+beta],
+          [-sinTheta*cosPhi,-sinPhi,-cosTheta*cosPhi,coix*sinTheta*cosPhi+coiy*sinPhi+coiz*cosTheta*cosPhi+d]];
+  let [xprime, yprime, zprime, pprime] = transform(Z, [x, y, z, p]);       
   return [x_mid + xprime, y_mid - yprime];
 }
 
@@ -49,7 +43,7 @@ function plot3DParametric(v, r, resolution = 100, color = 'hsl(190, 100%, 60%)',
   ctx.fillStyle = color;
   let [x_func, y_func, z_func] = v;
   for(let t = r[0]; t <= r[1]; t += (r[1] - r[0]) / resolution) {
-    let [rows, cols] = plot3D([x_func(t), y_func(t), z_func(t)], ep, coi, va);
+    let [rows, cols] = plot3D([x_func(t), y_func(t), z_func(t), 1], ep, coi, va);
     ctx.fillRect(cols, rows, 1, 1);    
   }
 }
@@ -79,24 +73,31 @@ function plot3DAxes(ep, coi, va) {
 // Plot a 3D coordinate system with some objects and animate a moving perspective (theta, phi)
 /*************************************************************************************************************************************************/
 async function paraboloid() {
+  let dia = 100;
+  let coi = [150, 150, 50];
+  let va = Math.PI/1.5;
+  
   while(true) {
-    for(let phi=0; phi<2*Math.PI; phi += Math.PI / 128) {
-      await new Promise(resolve => setTimeout(resolve, 20))
-      ctx.fillStyle = 'black';
-      ctx.fillRect(0,0,canvas.width,canvas.height);
+      for(let phi=0; phi<2 * Math.PI; phi += Math.PI / 128) {
+        await new Promise(resolve => setTimeout(resolve, 20))
+        ctx.fillStyle = 'black';
+        ctx.fillRect(0,0,canvas.width,canvas.height);
+        
+        let eyepoint = [dia*Math.cos(phi), dia*Math.sin(phi), 20];
+        plot3DAxes(eyepoint, coi, va);
+        plot3DGrid(eyepoint, coi, va);
 
-      plot3DAxes([150*Math.cos(phi), 150*Math.sin(phi), 10], [0,0,20], Math.PI/3);
-      plot3DGrid([150*Math.cos(phi), 150*Math.sin(phi), 10],[0,0,20], Math.PI/3);
-
-      for(let n = 0; n < 2 * Math.PI; n += Math.PI / 180) {
-        plot3DParametric([t => t * Math.cos(n), t => t * Math.sin(n), t => t*t/64], [0, 110], 20, 'goldenrod',[150*Math.cos(phi), 150*Math.sin(phi), 10],[0,0,20],Math.PI/3); // paraboloid
-        plot3DParametric([t => t * Math.cos(n), t => t * Math.sin(n), t => t], [-150, 0], 10, 'dodgerblue',[150*Math.cos(phi), 150*Math.sin(phi), 10], [0,0,20], Math.PI/3);  // cone
-        plot3DParametric([t => 150+t * Math.cos(n), t => 150+t * Math.sin(n), t => 50+(1000-t*t)**0.5], [0, 100], 80, 'orangered',[150*Math.cos(phi), 150*Math.sin(phi), 10],[0,0,20], Math.PI/3); // hemi-sphere
-        plot3DParametric([t => 150+t * Math.cos(n), t => 150+t * Math.sin(n), t => 50-((1000-t*t)**0.5)], [0, 100], 80, 'orangered',[150*Math.cos(phi), 150*Math.sin(phi), 10],[0,0,20], Math.PI/3); // hemi-sphere
+        for(let n = 0; n < 2 * Math.PI; n += Math.PI / 180) {
+          plot3DParametric([t => t * Math.cos(n), t => t * Math.sin(n), t => t*t/64], [0, 110], 20, 'goldenrod', eyepoint, coi, va); // paraboloid
+          plot3DParametric([t => t * Math.cos(n), t => t * Math.sin(n), t => t], [-150, 0], 10, 'dodgerblue', eyepoint, coi, va);  // cone
+          plot3DParametric([t => 150+t * Math.cos(n), t => 150+t * Math.sin(n), t => 50+(1000-t*t)**0.5], [0, 100], 80, 'orangered', eyepoint, coi, va); // hemi-sphere
+          plot3DParametric([t => 150+t * Math.cos(n), t => 150+t * Math.sin(n), t => 50-((1000-t*t)**0.5)], [0, 100], 80, 'orangered', eyepoint, coi, va); // hemi-sphere
+        }
       }
-    }
+    
   }
 }
+
 // plot3DAxes([1,0,1], [0,0,0], Math.PI/3);
 // plot3DGrid([1,0,1],[0,0,0], Math.PI/3);
 // for(let n = 0; n < 2* Math.PI; n += Math.PI/180) {
